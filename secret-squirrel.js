d73d1eb60c2315f3e6a39bb9fb48e0b59b9a16de
chee <chee@snoot.club>
2019-04-23 11:46:15 +0100

    Add rel-engage and the files it makes

---
+++ secret-squirrel.js
@@ -0,0 +1,14 @@
+module.exports = {
+	files: {
+		allow: [],
+		allowOverrides: [],
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			'chee@snoot\\.club', // package.json:17
+			'failoverarchitecturetype', // runbook.md.js:205
+			'keymanagementprocesstype', // runbook.md.js:217
+		],
+	},
+};

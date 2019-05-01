33621f3a5e6ddcee13572274c8236783c234b86f
chee <chee@snoot.club>
2019-05-01 14:26:39 +0100

    Silence the logger

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ const bizOpsSchema = require('@financial-times/biz-ops-schema');
 bizOpsSchema.configure({
 	baseUrl: process.env.SCHEMA_BASE_URL,
 	updateMode: 'stale',
+	logger: new Proxy(
+		{},
+		{
+			get() {
+				return Function.prototype;
+			},
+		},
+	),
 });
 
 function createBizopsNameNode() {

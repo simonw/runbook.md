bd172832007b6af29365026123b8973c39be350b
chee <chee@snoot.club>
2019-05-10 09:35:37 +0100

    Add tests for name parsing

---
+++ tests/name.test.js
@@ -0,0 +1,40 @@
+const runbookmd = require('..');
+const here = require('dedent');
+
+test('an h1 is parsed as name', async () => {
+	const { data, errors } = await runbookmd.parseRunbookString(
+		'# hello monkey',
+	);
+	expect(errors.length).toBe(0);
+	expect(data).toHaveProperty('name', 'hello monkey');
+});
+
+test('inline markdown is stripped', async () => {
+	const { data, errors } = await runbookmd.parseRunbookString(
+		"# Hello *monkey* _don't_ worry about a thing",
+	);
+	expect(errors.length).toBe(0);
+	expect(data).toHaveProperty(
+		'name',
+		"Hello monkey don't worry about a thing",
+	);
+});
+
+xtest('more than one h1 is an error', async () => {
+	const { errors } = await runbookmd.parseRunbookString(here`
+		# hello monkey
+
+		# also this
+	`);
+
+	expect(errors.length).toBe(1);
+});
+
+test('no h1 means no name', async () => {
+	const { data, errors } = await runbookmd.parseRunbookString(here`
+		wow
+	`);
+
+	expect(errors.length).toBe(0);
+	expect(data).toHaveProperty('name', undefined);
+});

aa2f474511ea480297aabc19b3e2df1472f67b0e
geoff thorpe <geoff.thorpe@ft.com>
2019-05-16 16:29:47 +0100

    Validate the parsed content via sos validate endpoint

--- src/validateHandler.js
+++ src/validateHandler.js
@@ -11,6 +11,66 @@ const responseHeaders = {
 	'Cache-Control': 'private, no-cache, no-store, must-revalidate, max-age=0',
 };
 
+const attemptParse = async (event, request) => {
+	const options = {
+		method: 'POST',
+		body: JSON.stringify(request),
+		headers: { Cookie: event.headers.Cookie },
+	};
+	const fetchResponse = await nodeFetch(
+		`${process.env.BASE_URL}/ingest`,
+		options,
+	);
+	if (!fetchResponse.ok) {
+		throw httpError(
+			fetchResponse.status,
+			`Attempt to access runbook.mk ingest failed with ${
+				fetchResponse.statusText
+			}`,
+		);
+	}
+	logger.info(
+		{ event: 'POSTED to runbook.mk /ingest endpoint', options },
+		'Waiting for validation response',
+	);
+	const result = await fetchResponse.json();
+	logger.info(
+		{ event: 'Result from runbook.mk /ingest endpoint', result },
+		'Validation response',
+	);
+	return result;
+};
+
+const attemptScore = async (event, request) => {
+	const options = {
+		method: 'POST',
+		body: JSON.stringify(request.content),
+		headers: { Cookie: event.headers.Cookie },
+	};
+	const fetchResponse = await nodeFetch(
+		`${process.env.SOS_URL}/validate`,
+		options,
+	);
+	if (!fetchResponse.ok) {
+		throw httpError(
+			fetchResponse.status,
+			`Attempt to access SOS ${
+				process.env.SOS_URL
+			}/validate failed with ${fetchResponse.statusText}`,
+		);
+	}
+	logger.info(
+		{ event: 'POSTED to /ingest endpoint', options },
+		'Waiting for SOS validation response',
+	);
+	const result = await fetchResponse.json();
+	logger.info(
+		{ event: 'Result from SOS /validate endpoint', result },
+		'SOS validate response',
+	);
+	return result;
+};
+
 const displayForm = async event => {
 	logger.info(
 		{ event: 'GET RUNBOOK-MD INGEST FORM', params: event },
@@ -26,33 +86,12 @@ const handleForm = async event => {
 	);
 	const formData = event.body;
 	const jsonFormData = querystring.parse(formData);
-	const options = {
-		method: 'POST',
-		body: JSON.stringify(jsonFormData),
-		headers: { Cookie: event.headers.Cookie },
-	};
-	const fetchResponse = await nodeFetch(
-		`${process.env.BASE_URL}/ingest`,
-		options,
-	);
-	if (!fetchResponse.ok) {
-		throw httpError(
-			fetchResponse.status,
-			`Attempt to access ingest failed with ${fetchResponse.statusText}`,
-		);
-	}
-	logger.info(
-		{ event: 'POSTED to /ingest endpoint', options },
-		'Waiting for validation response',
-	);
-	const result = await fetchResponse.json();
-	logger.info(
-		{ event: 'Result from /ingest endpoint', result },
-		'Validation response',
-	);
+
+	const parseResult = await attemptParse(event, jsonFormData);
+	const validateResult = await attemptScore(event, parseResult.data);
 	return {
 		statusCode: 200,
-		body: JSON.stringify(result),
+		body: JSON.stringify({ parseResult, validateResult }),
 		headers: responseHeaders,
 	};
 };

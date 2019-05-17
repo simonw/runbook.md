f901c95562e67e95f5ea87ddfb50d1e75d69e48d
geoff thorpe <geoff.thorpe@ft.com>
2019-05-17 09:21:05 +0100

    Write to BizOps if option is set and apikey is supplied.

---
+++ src/lib/external-apis.js
@@ -0,0 +1,76 @@
+const logger = require('@financial-times/lambda-logger');
+const httpError = require('http-errors');
+const nodeFetch = require('node-fetch');
+
+const callExternalApi = async ({ name, url, payload, headers }) => {
+	const options = {
+		method: 'POST',
+		body: JSON.stringify(payload),
+		headers,
+	};
+	const fetchResponse = await nodeFetch(url, options);
+	if (!fetchResponse.ok) {
+		throw httpError(
+			fetchResponse.status,
+			`Attempt to access ${name} ${url} failed with ${
+				fetchResponse.statusText
+			}`,
+		);
+	}
+	logger.info(
+		{ event: `POSTED to ${url}`, options },
+		`Waiting for ${name} response`,
+	);
+	return fetchResponse.json();
+};
+
+const attemptParse = async (event, request) => {
+	return callExternalApi({
+		name: 'RUNBOOK.MD parse',
+		url: `${process.env.BASE_URL}/ingest`,
+		payload: request,
+		headers: { Cookie: event.headers.Cookie },
+	});
+};
+
+const attemptScore = async (event, request) => {
+	return callExternalApi({
+		name: 'SOS validate',
+		url: `${process.env.SOS_URL}/validate`,
+		payload: request,
+		headers: { Cookie: event.headers.Cookie },
+	});
+};
+
+const updateBizOps = async (event, apiKey, systemCode, content) => {
+	const headers = {
+		api_key: apiKey,
+		'x-api-key': apiKey,
+		'client-id': 'biz-ops-runbook-md',
+		'content-type': 'application/json',
+		'client-user-id': event.clientUserId,
+	};
+	const queryString = '?relationshipAction=merge';
+	const url = `${process.env.BIZ_OPS_API_URL}/v2/node/System/${systemCode}`;
+	const options = {
+		method: 'POST',
+		headers,
+		body: JSON.stringify(content),
+	};
+	const response = await nodeFetch(`${url}${queryString}`, options);
+	if (!response.ok) {
+		throw httpError(
+			response.status,
+			`Attempt to access BIZ_OPS ${url} failed with ${
+				response.statusText
+			}`,
+		);
+	}
+	return { response: response.json(), status: response.status };
+};
+
+module.exports = {
+	attemptParse,
+	attemptScore,
+	updateBizOps,
+};
--- src/validateHandler.js
+++ src/validateHandler.js
@@ -1,74 +1,18 @@
 const logger = require('@financial-times/lambda-logger');
 const httpError = require('http-errors');
-const nodeFetch = require('node-fetch');
 const querystring = require('qs');
 const response = require('./lib/response');
 const { createLambda } = require('./lib/lambda');
 const template = require('./templates/validate-page');
+const {
+	attemptParse,
+	// attemptScore,
+	updateBizOps,
+} = require('./lib/external-apis');
 
 const responseHeaders = {
 	'Content-Type': 'application/json',
 	'Cache-Control': 'private, no-cache, no-store, must-revalidate, max-age=0',
-};
-
-const attemptParse = async (event, request) => {
-	const options = {
-		method: 'POST',
-		body: JSON.stringify(request),
-		headers: { Cookie: event.headers.Cookie },
-	};
-	const fetchResponse = await nodeFetch(
-		`${process.env.BASE_URL}/ingest`,
-		options,
-	);
-	if (!fetchResponse.ok) {
-		throw httpError(
-			fetchResponse.status,
-			`Attempt to access runbook.mk ingest failed with ${
-				fetchResponse.statusText
-			}`,
-		);
-	}
-	logger.info(
-		{ event: 'POSTED to runbook.mk /ingest endpoint', options },
-		'Waiting for validation response',
-	);
-	const result = await fetchResponse.json();
-	logger.info(
-		{ event: 'Result from runbook.mk /ingest endpoint', result },
-		'Validation response',
-	);
-	return result;
-};
-
-const attemptScore = async (event, request) => {
-	const options = {
-		method: 'POST',
-		body: JSON.stringify(request.content),
-		headers: { Cookie: event.headers.Cookie },
-	};
-	const fetchResponse = await nodeFetch(
-		`${process.env.SOS_URL}/validate`,
-		options,
-	);
-	if (!fetchResponse.ok) {
-		throw httpError(
-			fetchResponse.status,
-			`Attempt to access SOS ${
-				process.env.SOS_URL
-			}/validate failed with ${fetchResponse.statusText}`,
-		);
-	}
-	logger.info(
-		{ event: 'POSTED to /ingest endpoint', options },
-		'Waiting for SOS validation response',
-	);
-	const result = await fetchResponse.json();
-	logger.info(
-		{ event: 'Result from SOS /validate endpoint', result },
-		'SOS validate response',
-	);
-	return result;
 };
 
 const displayForm = async event => {
@@ -88,10 +32,27 @@ const handleForm = async event => {
 	const jsonFormData = querystring.parse(formData);
 
 	const parseResult = await attemptParse(event, jsonFormData);
-	const validateResult = await attemptScore(event, parseResult.data);
+	const validateResult = {}; // await attemptScore(event, parseResult.data);
+	let writeResult = {};
+	if (jsonFormData.writeToBizOps) {
+		if (jsonFormData.apiKey) {
+			writeResult = await updateBizOps(
+				event,
+				jsonFormData.apiKey,
+				jsonFormData.systemCode,
+				parseResult.data,
+			);
+		} else {
+			writeResult = { BizOpsError: 'Please supply a Biz Ops API Key' };
+		}
+	} else {
+		writeResult = {
+			BizOpsUnchanged: 'The data has not been written to Biz Ops',
+		};
+	}
 	return {
 		statusCode: 200,
-		body: JSON.stringify({ parseResult, validateResult }),
+		body: JSON.stringify({ parseResult, validateResult, writeResult }),
 		headers: responseHeaders,
 	};
 };

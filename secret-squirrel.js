00c3218cce75c706a8b4538224a3b8caef960641
chee <chee@snoot.club>
2019-05-29 12:13:31 +0100

    Add readme image uuid to secret squirrel

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -6,9 +6,8 @@ module.exports = {
 	strings: {
 		deny: [],
 		denyOverrides: [
+			'b62f4580-81f6-11e9-8f9b-7d694d159e85', // README.md:9
 			'chee@snoot\\.club', // package.json:17
-			'failoverarchitecturetype', // runbook.md.js:205
-			'keymanagementprocesstype', // runbook.md.js:217
 		],
 	},
 };

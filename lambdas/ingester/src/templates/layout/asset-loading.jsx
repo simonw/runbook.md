f7f6955ac2bc67bb8b97d13ecd034b2b14c38dc0
doramatadora <dora.militaru@ft.com>
2019-06-24 16:59:05 +0100

    fix manifest path

--- lambdas/ingester/src/templates/layout/asset-loading.jsx
+++ lambdas/ingester/src/templates/layout/asset-loading.jsx
@@ -4,7 +4,7 @@ const config = require('../../lib/config');
 const webpackManifest = config.get('IS_LAMBDA')
 	? // built resource, so doesn't exist in local dev
 	  // eslint-disable-next-line import/no-unresolved
-	  require('../../browser/manifest.json')
+	  require('../../../dist/browser/manifest.json')
 	: {
 			'main.css': 'main.css',
 			'main.js': 'main.js',

15b18e52a8db17c1771ed6ffd95326b8572760b4
doramatadora <dora.militaru@ft.com>
2019-06-28 12:59:25 +0100

    try something new

--- lambdas/ingester/src/githubWebhookHandler.js
+++ lambdas/ingester/src/githubWebhookHandler.js
@@ -1,8 +1,10 @@
 const { serverless } = require('@probot/serverless-lambda');
 const { webhookListener } = require('./commands/webhook');
 
+const privateKey = process.env.GITHUB_PRIVATE_KEY_STRING;
+
 process.env.APP_ID = process.env.GITHUB_APP_ID;
-process.env.PRIVATE_KEY = JSON.parse(process.env.GITHUB_PRIVATE_KEY_STRING);
+process.env.PRIVATE_KEY = privateKey.replace('"', '').replace(/\\n/g, '\n');
 process.env.WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET;
 
 exports.handler = serverless(webhookListener);
